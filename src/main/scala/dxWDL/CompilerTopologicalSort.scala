8df8634624a4b1dcd5506593bf5de0a8e62ea384
Geet Duggal <geetduggal@gmail.com>
2017-05-26 02:39:17 +0000

    WIP.  Works with recursive sorting.  Need to clean up algo significantly though.

--- src/main/scala/dxWDL/CompilerTopologicalSort.scala
+++ src/main/scala/dxWDL/CompilerTopologicalSort.scala
@@ -71,9 +71,25 @@ object CompilerTopologicalSort {
         tsort(toPred, Seq())
     }
 
+    // Internal procedure to sort children nodes at a particular level in the AST
+    def tsortASTnodes(nodes: Seq[Scope], cState:State, recursionDepth: Int) : Seq[Scope] = {
+        val indent = " "*recursionDepth*4
+        /*
+        // Recursively sort nested scatters as they can have the same kind of
+        // structure as a workflow
+        val recursedNodes = nodes.map {
+            case scatter : Scatter => {
+                val newScatter = Scatter(scatter.index, scatter.item, scatter.collection, scatter.ast)
+                Utils.trace(cState.verbose, indent+"Recursively performing toological sort on scatter: " + scatter.fullyQualifiedName)
+                newScatter.children = tsortASTnodes(scatter.children, cState, recursionDepth + 1)
+                Utils.trace(cState.verbose, indent+"End recursion")
+                newScatter.asInstanceOf[GraphNode]
+            }
+            case anyOtherNode => anyOtherNode.asInstanceOf[GraphNode]
+        }
+        */
+        val recursedNodes = nodes.map { x => x.asInstanceOf[GraphNode] }
 
-    // WDL-specific wrapper for topological sorting
-    def sortWorkflow(wf: Workflow, taskLines: Vector[String], cState:State) : Vector[String] = {
         // Create workflow graph sequence of edges for topological sorting
         // Nodes in the graph (Scopes) can be a call, a scatter, or a declaration
 
@@ -81,18 +97,22 @@ object CompilerTopologicalSort {
         // node it belongs to. This allows for a root level scatter node to be placed
         // after its last dependency.
 
-        val scatterRoot : Map[Scope, Scatter] = wf.children.map {
-            case scat: Scatter => scat.descendants.map { d => (d, scat) }
-            case _ => Set[(Scope, Scatter)]()
+        val scatterRoot : Map[GraphNode, Scatter] = recursedNodes.map {
+            case scat: Scatter => scat.descendants.map { d => (d.asInstanceOf[GraphNode], scat) }
+            case _ => Set[(GraphNode, Scatter)]()
         }.flatten.toMap
+
+        val graphNodes = recursedNodes.head.parent.get match {
+            case node : GraphNode => node.descendants.map { x => x.asInstanceOf[GraphNode] } + node
+            case workflow : Workflow => workflow.descendants.map { x => x.asInstanceOf[GraphNode] }
+            case anyOtherNode => throw new Exception("Unexpected parent type")
+        }
 
         // Generate a list of edges corresponding to dependencies for this call
         // e.g. suppose call x has dependencies a,b,c.  This procedure will generate:
         // [(a,x), (b,x), (c,x)]
-        val edges : Set[(Scope, Scope)] = wf.children.map { node =>
-            val gnode = node.asInstanceOf[GraphNode]
-
-            val nodeParents : Vector[(Scope, Scope)] = gnode.upstream.map { parent =>
+        val edges : Set[(Scope, Scope)] = recursedNodes.map { gnode =>
+            val nodeParents : Vector[(Scope, Scope)] = (gnode.upstream & graphNodes).map { parent =>
                val parentActual : Scope =
                    // If any node's parent is a descendant of a root
                    // level scatter, use the scatter as the parent
@@ -106,39 +126,67 @@ object CompilerTopologicalSort {
                (parentActual, gnode.asInstanceOf[Scope])
             }.toVector
 
-            val descendantParents : Vector[(Scope, Scope)] =
-                // If a scatter's descendants have parents outside the scatter context,
-                // map the scatter itself to them.
-                if (node.isInstanceOf[Scatter]) {
-                    val nodeDescendants = node.descendants.map { d => d.asInstanceOf[GraphNode] }
-                    val upstreamParents = nodeDescendants.map { d => d.upstream }.flatten.toSet
-                    ((upstreamParents -- nodeDescendants) - node.asInstanceOf[GraphNode]).map{ parent =>
+            val descendantParents : Vector[(Scope, Scope)] = gnode match {
+                case scatter: Scatter => {
+                    // If a scatter's descendants have parents outside the scatter context,
+                    // map the scatter itself to them.
+                    val nodeDescendants = scatter.descendants.map { d => d.asInstanceOf[GraphNode] }
+                    val upstreamParents = nodeDescendants.map { d => d.upstream }.flatten.toSet & graphNodes
+                    ((upstreamParents -- nodeDescendants) - gnode).map{ parent =>
                         // If the parent outside the scatter is also a member of a scatter, use it instead.
-                        if ( scatterRoot.contains(parent) ) {
-                            (scatterRoot(parent), node)
+                        if ( scatterRoot.contains(parent) && gnode != scatterRoot(parent)) {
+                            (scatterRoot(parent), gnode)
                         } else {
-                            (parent, node)
+                            (parent, gnode)
                         }
                     }.toVector
-                } else {
-                    Vector[(Scope, Scope)]()
                 }
+                case anyOtherNode => Vector[(Scope, Scope)]()
+            }
 
             nodeParents ++ descendantParents
         }.flatten.toSet
+
+        // Print out edges of DAG for debugging purposes
         edges.foreach {
-            case (v,w) => Utils.trace(true, v.asInstanceOf[GraphNode].fullyQualifiedName + "-->" + w.asInstanceOf[GraphNode].fullyQualifiedName)
+            case (v,w) => Utils.trace(cState.verbose, indent + v.asInstanceOf[GraphNode].fullyQualifiedName + " -> " + w.asInstanceOf[GraphNode].fullyQualifiedName)
         }
 
         // Topologically sort graph or return error that workflow contains a cycle
-        val sortedNodes = tsort(edges)
+        val sortedNodes = tsort(edges).toSeq
+        Utils.trace(cState.verbose,
+            indent + "Sorted nodes: " + sortedNodes.map { node => node.asInstanceOf[GraphNode].fullyQualifiedName }.mkString(", "))
+
+
+        sortedNodes.filter(x=>nodes.contains(x))
+    }
+
+    def tsortScatterPrint(ssc: Scatter, level: Int, cState: State) : Vector[String] = {
+        val top: String = s"scatter (${ssc.item} in ${ssc.collection.toWdlString})"
+        Utils.trace(true, "Children: " + ssc.children.map { node => node.asInstanceOf[GraphNode].fullyQualifiedName }.mkString(", "))
+        val sortedChildren = tsortASTnodes(ssc.children, cState, level + 1)
+        Utils.trace(true, "Sorted Children: " + sortedChildren.map { node => node.asInstanceOf[GraphNode].fullyQualifiedName }.mkString(", "))
+        val children = sortedChildren.map {
+        //val children = ssc.children.map{
+            case x:Call => WdlPrettyPrinter.apply(x, level + 1)
+            case x:Declaration => WdlPrettyPrinter.apply(x, level + 1)
+            case x:Scatter => tsortScatterPrint(x, level + 1, cState)
+            case _ => throw new Exception("Unimplemented scatter element")
+        }.flatten.toVector
+        WdlPrettyPrinter.buildBlock(top, children.toVector, level)
+    }
+
+    // WDL-specific wrapper for topological sorting
+    def sortWorkflow(wf: Workflow, taskLines: Vector[String], cState:State) : Vector[String] = {
+        val sortedNodes = tsortASTnodes(wf.children, cState, 0)
+        Utils.trace(true, "Sorted top level: " + sortedNodes.map { node => node.asInstanceOf[GraphNode].fullyQualifiedName }.mkString(", "))
 
         // pretty print the workflow. The output
         // must be readable by the standard WDL compiler.
         val elemsPp : Vector[String] = sortedNodes.map {
             case call: Call => WdlPrettyPrinter.apply(call, 1)
             case decl: Declaration => WdlPrettyPrinter.apply(decl, 1)
-            case ssc: Scatter => WdlPrettyPrinter.apply(ssc, 1)
+            case ssc: Scatter => tsortScatterPrint(ssc, 1, cState)
             case x =>
                 throw new Exception(cState.cef.notCurrentlySupported(x.ast,
                                                                      "workflow element"))
