108ec936479fc8427e5b443901a57cc28781b36b
Ohad Rodeh <orodeh@dnanexus.com>
2019-01-25 19:32:16 -0800

    WIP

--- scripts/compiler_image/runc.sh
+++ scripts/compiler_image/runc.sh
@@ -1,3 +1,3 @@
 #!/bin/bash -e
 eval "$(dx env --bash)"
-sudo docker run -e DX_SECURITY_CONTEXT="$DX_SECURITY_CONTEXT" -v $pwd:/tmp/workdir dnanexus/dxwdl "$@"
+sudo docker run --rm -e DX_SECURITY_CONTEXT="$DX_SECURITY_CONTEXT" -v $pwd:/tmp/workdir dnanexus/dxwdl "$@"
