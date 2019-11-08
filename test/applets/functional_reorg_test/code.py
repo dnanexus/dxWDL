98d14d7849e54cac3b56d64d12008a8ceca83971
xiuchengquek <xquek@dnanexus.com>
2019-11-08 13:24:53 -0800

    refactor sample reorg app to find files from job id

--- test/applets/functional_reorg_test/code.py
+++ test/applets/functional_reorg_test/code.py
@@ -12,26 +12,30 @@ def create_folder(container, folder_name):
 
 
 @dxpy.entry_point('main')
-def main(output_file, output_config_file, ___reorg_config=None, ___reorg_status=None):
+def main(___reorg_config=None, ___reorg_status=None):
 
-    output_file = dxpy.DXFile(output_file)
-    output_config_file = dxpy.DXFile(output_config_file)
+    job_describe = dxpy.describe(dxpy.DX_JOB_ID)
+    analysis_id = job_describe["analysis"]
+
+    stages = dxpy.describe(analysis_id)["stages"]
+
+    # key is the name of the output and the value is the link of the file.
+    output_map = [x['execution']['output'] for x in stages if x['id'] == 'stage-outputs'][0]
+
+    output_file = output_map.get('output_file')
+    output_config_file = output_map.get('output_config_file')
 
     output_folder_1 = '/tests/test_reorg/out_1'
     output_folder_2 = '/tests/test_reorg/out_2'
 
     dx_container = dxpy.DXProject(dxpy.PROJECT_CONTEXT_ID)
 
-    create_folder(dx_container, output_folder_1)
-    create_folder(dx_container, output_folder_2)
-
     dx_container.move(
         destination=output_folder_1,
-        objects=[output_file.get_id()]
+        objects=[output_file]
     )
-
     dx_container.move(
-        objects=[output_config_file.get_id()],
+        objects=[output_config_file],
         destination=output_folder_2,
     )
 
