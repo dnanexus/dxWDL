f666a08847693a24ad1a9a62ab22fa90f0a4696c
Ohad Rodeh <orodeh@dnanexus.com>
2020-05-13 15:52:04 -0700

    Copying files from the wdlTools repository instead of copying the jar file

--- scripts/copy_wdlTools_sources.sh
+++ scripts/copy_wdlTools_sources.sh
@@ -5,9 +5,9 @@ DXWDL_ROOT=$CURRENT_DIR/..
 WDL_TOOLS_ROOT=$DXWDL_ROOT/../wdlTools
 
 rm -rf $DXWDL_ROOT/src/main/scala/wdlTools
-rm -rf $DXWDL_ROOT/src/main/antlr4
+rm -rf $DXWDL_ROOT/src/main/java/Wdl*
 mkdir $DXWDL_ROOT/src/main/scala/wdlTools
 
 cp $WDL_TOOLS_ROOT/Makefile .
-cp -r $WDL_TOOLS_ROOT/src/main/antlr4 $DXWDL_ROOT/src/main/
+cp -r $WDL_TOOLS_ROOT/src/main/java/Wdl* $DXWDL_ROOT/src/main/java/
 cp -r $WDL_TOOLS_ROOT/src/main/scala/wdlTools/{eval,syntax,types,util} $DXWDL_ROOT/src/main/scala/wdlTools
