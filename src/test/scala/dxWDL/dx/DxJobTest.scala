135a9cb3d4315a20a66014144eefbf22b5b4bdfe
Marek Hrvol <mhrvol@seznam.cz>
2020-07-01 15:11:54 +0200

    updated unit test to contain only dxWDL playground project

--- src/test/scala/dxWDL/dx/DxJobTest.scala
+++ src/test/scala/dxWDL/dx/DxJobTest.scala
@@ -6,13 +6,13 @@ import org.scalatest.matchers.should.Matchers
 class DxJobTest extends AnyFlatSpec with Matchers  {
 
   it should "describe job" in {
-    val dxAppJob = DxJob("job-FqpyZP00k25Q2Kp5B0JV8Yv4", Some(DxProject("project-FpGpg8Q0k25YQ1k53YF8YxkG")))
+    val dxAppJob = DxJob("job-Fqy8YF00ffPB148G5f490Y25", Some(DxProject("project-FGpfqjQ0ffPF1Q106JYP2j3v")))
     val dxAppJobDescription = dxAppJob.describe()
-    dxAppJobDescription.executable.getId shouldBe "app-Fqppx0j0JBgZ4XbQB6Q6jVQG"
+    dxAppJobDescription.executable.getId shouldBe "app-Fqy8Xx00zZvZ503g5gJgq3Gb"
 
-    val dxAppletJob = DxJob("job-Fqpyv100k25Q2Kp5B0JV8Z1f", Some(DxProject("project-FpGpg8Q0k25YQ1k53YF8YxkG")))
+    val dxAppletJob = DxJob("job-Fqy8XV00ffP3b1j75bvVFX3b", Some(DxProject("project-FGpfqjQ0ffPF1Q106JYP2j3v")))
     val dxAppletJobDescription = dxAppletJob.describe()
-    dxAppletJobDescription.executable.getId shouldBe "applet-Fqp89780k25vBB5J6x2jbJvJ"
+    dxAppletJobDescription.executable.getId shouldBe "applet-Fqy8Vy80ffP3b1j75bvVFX3Q"
   }
 
 }
