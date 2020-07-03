5abde831765153fd21401435da743ef7343957e2
Marek Hrvol <mhrvol@seznam.cz>
2020-07-03 10:06:54 +0200

    updated test

--- src/test/scala/dxWDL/dx/DxJobTest.scala
+++ src/test/scala/dxWDL/dx/DxJobTest.scala
@@ -5,14 +5,16 @@ import org.scalatest.matchers.should.Matchers
 
 class DxJobTest extends AnyFlatSpec with Matchers {
 
+  val dxWDLPlaygroundProject = Some(DxProject("project-FGpfqjQ0ffPF1Q106JYP2j3v"))
+
   it should "describe job which ran app" in {
-    val dxAppJob = DxJob("job-Fqy8YF00ffPB148G5f490Y25", Some(DxProject("project-FGpfqjQ0ffPF1Q106JYP2j3v")))
+    val dxAppJob = DxJob("job-Fqy8YF00ffPB148G5f490Y25", dxWDLPlaygroundProject)
     val dxAppJobDescription = dxAppJob.describe()
     dxAppJobDescription.executable.getId shouldBe "app-Fqy8Xx00zZvZ503g5gJgq3Gb"
   }
 
   it should "describe job which ran applet" in {
-    val dxAppletJob = DxJob("job-Fqy8XV00ffP3b1j75bvVFX3b", Some(DxProject("project-FGpfqjQ0ffPF1Q106JYP2j3v")))
+    val dxAppletJob = DxJob("job-Fqy8XV00ffP3b1j75bvVFX3b", dxWDLPlaygroundProject)
     val dxAppletJobDescription = dxAppletJob.describe()
     dxAppletJobDescription.executable.getId shouldBe "applet-Fqy8Vy80ffP3b1j75bvVFX3Q"
   }
