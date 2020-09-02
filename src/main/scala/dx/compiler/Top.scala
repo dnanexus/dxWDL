6a3c3432d174077fdb7c653058bb4fc7d63ad66d
Tomas Mikula <tomas.mikula@gmail.com>
2020-09-02 19:28:47 +0200

    Simplify a pattern match in Top.scala (#440)
    * Eliminate redundant pattern match.
    
    The code pattern was
    
        cResults.primaryCallable match {
          case Some(wf) if execTree.isDefined =>
            cResults.primaryCallable match { // this is the redundant pattern match
              case None =>
                // dead code
              case Some(primary) =>
                // we already know that primary = wf
    
    * Remove unnecessary alias `val primary = wf`.
    
    * Factor common subexpression out of two branches.
    
    Also avoids the unsafe Option#get operation.
    
    Schematically, the change is (with x being the common subexpression):
    
      Before:
    
          case Some(wf) if execTree.isDefined =>
            val y = f(execTree.get) // Safe get because we check isDefined above
            (x, Some(y))
          case Some(wf) =>
            (x, None)
    
      After:
    
          case Some(wf) =>
            val yOpt = execTree.map(f)
            (x, yOpt)

--- src/main/scala/dx/compiler/Top.scala
+++ src/main/scala/dx/compiler/Top.scala
@@ -318,23 +318,18 @@ case class Top(cOpt: CompilerOptions) {
       case None =>
         val ids = cResults.execDict.map { case (_, r) => r.dxExec.getId }.mkString(",")
         (ids, None)
-      case Some(wf) if execTree.isDefined =>
-        cResults.primaryCallable match {
-          case None =>
-            (wf.dxExec.getId, None)
-          case Some(primary) =>
-            val tree = new Tree(cResults.execDict)
-            val treeRepr = execTree.get match { // Safe get because we check isDefined above
-              case PrettyTreePrinter =>
-                Left(
-                    Tree.generateTreeFromJson(tree.apply(primary).asJsObject)
-                )
-              case JsonTreePrinter => Right(tree.apply(primary)) // Convert to string
-            }
-            (wf.dxExec.getId, Some(treeRepr))
-        }
       case Some(wf) =>
-        (wf.dxExec.getId, None)
+        val treeReprOpt = execTree.map { treePrinter =>
+          val tree = new Tree(cResults.execDict)
+          treePrinter match {
+            case PrettyTreePrinter =>
+              Left(
+                  Tree.generateTreeFromJson(tree.apply(wf).asJsObject)
+              )
+            case JsonTreePrinter => Right(tree.apply(wf)) // Convert to string
+          }
+        }
+        (wf.dxExec.getId, treeReprOpt)
     }
   }
 }
